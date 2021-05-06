114e7b42022b959da53716584657aa9bac7f3e6d
Jaime Adalberto Lomeli <lomelijaime01@gmail.com>
2021-05-06 12:36:44 -0500

    prueba integración abril

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+function ejecuta_consulta()
+    {
+        $conections = mysqli_connect('localhost', 'root', '', 'pruebablg');
+        if (!$conections) die("Error");
+
+        mysqli_query($conections,"SET NAMES 'utf8'");
+        $consulta = "SELECT * FROM post";
+        $resultado = mysqli_query($conections, $consulta);
+        $filas = array();
+
+        while ($fila = mysqli_fetch_array($resultado, MYSQLI_ASSOC)) {
+            $filas[] = $fila;
+        }
+        mysqli_close($conections);
+    return $filas;
+
+
+    }
+?>
 <!DOCTYPE html>
 <html lang="es">
 
@@ -51,6 +72,25 @@
                 notable la diferencia entre las clases sociales.
             </p>
             <h2 id="Aprende" class="alinear">Aprende</h2>
+            <div class="grid-aprende">
+            <?php 
+            $posts = ejecuta_consulta();
+            $cont = 0;
+            foreach($posts as $post){
+            ?>
+                    <div class="card-aprende">
+                        <img src="<?php echo $post['Image']; ?>" alt="aprendefinanzas img" class="card-img-top img-fluid">
+                            <div class="">
+                                <h3 class=""><?php echo $post['Title']; ?></h3>
+                                <p class=""><?php echo $post['Date']; ?></p>
+                                <div class="flex-align--der">
+                                    <a href="/aprendefinanzas/recurso/<?php echo $post['ID_Post']; ?>" class="boton w-sm-total">Ver más</a>
+                            </div>
+                        </div>
+                    </div>
+                <?php }?>
+                </div>
+            </div>
             <h2 id="Contacto" class="alinear">Contacto</h2>
             <form action="#" class="formulario">
                 <fieldset>
