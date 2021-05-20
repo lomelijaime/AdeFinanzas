dd437760498468ec4d7829718ea0031ca10b5bcc
Jaime Adalberto Lomeli <lomelijaime01@gmail.com>
2021-05-20 13:55:20 -0500

    Changes 20May

--- index.php
+++ index.php
@@ -1,23 +1,21 @@
 <?php
 header('Content-Type: text/html; charset=UTF-8');
 function ejecuta_consulta()
-    {
-        $conections = mysqli_connect('localhost', 'root', '', 'pruebablg');
-        if (!$conections) die("Error");
+{
+    $conections = mysqli_connect('localhost', 'root', '', 'pruebablg');
+    if (!$conections) die("Error");
 
-        mysqli_query($conections,"SET NAMES 'utf8'");
-        $consulta = "SELECT * FROM post";
-        $resultado = mysqli_query($conections, $consulta);
-        $filas = array();
+    mysqli_query($conections, "SET NAMES 'utf8'");
+    $consulta = "SELECT * FROM post";
+    $resultado = mysqli_query($conections, $consulta);
+    $filas = array();
 
-        while ($fila = mysqli_fetch_array($resultado, MYSQLI_ASSOC)) {
-            $filas[] = $fila;
-        }
-        mysqli_close($conections);
-    return $filas;
-
-
+    while ($fila = mysqli_fetch_array($resultado, MYSQLI_ASSOC)) {
+        $filas[] = $fila;
     }
+    mysqli_close($conections);
+    return $filas;
+}
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -73,55 +71,50 @@ function ejecuta_consulta()
             </p>
             <h2 id="Aprende" class="alinear">Aprende</h2>
             <div class="grid-aprende">
-            <?php 
-            $posts = ejecuta_consulta();
-            $cont = 0;
-            foreach($posts as $post){
-            ?>
+                <?php
+                $posts = ejecuta_consulta();
+                foreach ($posts as $post) {
+                ?>
                     <div class="card-aprende">
-                        <img src="<?php echo $post['Image']; ?>" alt="aprendefinanzas img" class="card-img-top img-fluid">
-                            <div class="">
-                                <h3 class=""><?php echo $post['Title']; ?></h3>
-                                <p class=""><?php echo $post['Date']; ?></p>
-                                <div class="flex-align--der">
-                                    <a href="/aprendefinanzas/recurso/<?php echo $post['ID_Post']; ?>" class="boton w-sm-total">Ver más</a>
+                        <img src="<?php echo $post['Image']; ?>" alt="aprendefinanzas img" class="card-img">
+                        <div class="card-contenido">
+                            <h3 class=""><?php echo $post['Title']; ?></h3>
+                            <p class=""><?php echo $post['Date']; ?></p>
+                            <a href="/aprendefinanzas/recurso/<?php echo $post['ID_Post']; ?>" class="boton w-sm-total card-btn">Ver más</a>
+                        </div>
+                    </div>
+                <?php } ?>
+            </div>
+            <div>
+                <h2 id="Contacto" class="alinear">Contacto</h2>
+
+                <form action="#" class="formulario">
+                    <fieldset>
+                        <legend>Contáctanos llenando todos los campos</legend>
+                        <div class="contenedor-campos">
+                            <div class="campo">
+                                <label>Nombre</label>
+                                <input class="texto-campo" type="text" name="nombre" placeholder="Tu Nombre" required autocomplete="off" />
+                            </div>
+                            <div class="campo">
+                                <label>Teléfono</label>
+                                <input class="texto-campo" type="tel" name="telefono" placeholder="Tu Teléfono" required autocomplete="off" />
+                            </div>
+                            <div class="campo">
+                                <label>Correo Electrónico</label>
+                                <input class="texto-campo" type="email" name="email" placeholder="Tu E-mail" required autocomplete="email" />
+                            </div>
+                            <div class="campo">
+                                <label>Contenido</label>
+                                <textarea class="texto-campo text-md" autocomplete="off" placeholder="Redacta aquí tu mensaje"></textarea>
                             </div>
                         </div>
-                    </div>
-                <?php }?>
-                </div>
+                        <div class="flex-align--der">
+                            <input class="boton w-sm-total" type="submit" value="Enviar" />
+                        </div>
+                    </fieldset>
+                </form>
             </div>
-            <h2 id="Contacto" class="alinear">Contacto</h2>
-            <form action="#" class="formulario">
-                <fieldset>
-                    <legend>Contáctanos llenando todos los campos</legend>
-                    <div class="contenedor-campos">
-                        <div class="campo">
-                            <label>Nombre</label>
-                            <input class="texto-campo" type="text" name="nombre" placeholder="Tu Nombre" required autocomplete="off" />
-                        </div>
-
-                        <div class="campo">
-                            <label>Teléfono</label>
-                            <input class="texto-campo" type="tel" name="telefono" placeholder="Tu Teléfono" required autocomplete="off" />
-                        </div>
-
-                        <div class="campo">
-                            <label>Correo Electrónico</label>
-                            <input class="texto-campo" type="email" name="email" placeholder="Tu E-mail" required autocomplete="email" />
-                        </div>
-
-                        <div class="campo">
-                            <label>Contenido</label>
-                            <textarea class="texto-campo text-md" autocomplete="off" placeholder="Redacta aquí tu mensaje"></textarea>
-                        </div>
-                    </div>
-
-                    <div class="flex-align--der">
-                        <input class="boton w-sm-total" type="submit" value="Enviar" />
-                    </div>
-                </fieldset>
-            </form>
         </div>
     </main>
     <?php require './assets/src/php/minFooter.php' ?>
